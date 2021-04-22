87f9c6860b3601d247df794907bdc0979990133c
Brianna McGovern <bmcgovern007@gmail.com>
2021-04-21 21:10:52 -0700

    clean up

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-const electron = require("electron");
-  
-// Importing BrowserWindow from Main Process 
-// using Electron remote
-// const BrowserWindow = electron.remote.BrowserWindow;
-// const win = BrowserWindow.getFocusedWindow();
-  
-// // let win = BrowserWindow.getAllWindows()[0];
-  
-// win.webContents.on("before-input-event", (event, input) => {
-//     console.log(input);
-// });
-
-// var charfield = document.getElementById("char");
-// charfield.onkeypress = function(e) {
-//     e = e || window.event;
-//     var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
-//     if (charCode > 0) {
-//         alert("Typed character: " + String.fromCharCode(charCode));
-//     }
-// };
-
 function forClear() {
     document.getElementById("output").innerHTML = "0";
     clearHistory();
@@ -53,9 +31,6 @@ function solve() {
 }
 
 function history(equation, solved) {
-    // if ((document.getElementById("equation").innerHTML).find(element => element == '=')) {
-    //     document.getElementById("equation").innerHTML += equation + ' = ' + solved;
-    // }
     document.getElementById("equation").innerHTML = equation + ' = ' + solved;
 }
 
